564c9b8656475babc51efc19a575a42cf61fa898
Wanglala <sue4100035045@gmail.com>
2020-05-27 12:37:15 +0800

    fix: modify app.js

--- app.js
+++ app.js
@@ -15,10 +15,27 @@ app.use('/', function (req, res, next) {
   const start = `${year}-${month}-${date} ${hour}:${min}:${sec}`
   const from = `${req.method} from ${req.originalUrl}`
 
+  // //日期的另一種寫法，上面那一串可改成下面這樣
+  // const option = {       //oprtion要有timeZone(e.g.zh-TW)
+  //   hour12: false,       //24小時制
+  // }
+  // const startTime2 = new Date().toLocaleString('zh-TW', option)  //en-TW會變成/分隔
+  // console.log(startTime2)  //e.g. 2020-5-27 12:15:39
+
+
+
+  // //利用console.time和console.timeEnd計算經過時間 
+  // //from 同學作業 https://github.com/Mia-Chen-taiwan/middleware-practice/blob/master/app.js
+  // console.time(`${startTime.toLocaleString()} | ${from} | total time`)
+  // res.on('finish', () => {
+  //   console.timeEnd(`${startTime.toLocaleString()} | ${ from} | total time`)
+  // })
+
   res.on('finish', () => {
-    const finish = new Date().getMilliseconds()
-    const within = finish - ms
-    console.log(`${start} | ${from} | total time: ${within}ms`)
+    const finish = new Date()
+    const within = finish - startTime
+    //console.timeEnd(`${startTime.toLocaleString()} | ${ from} | total time`)
+    //console.log(`${start} | ${from} | total time: ${within}ms`)
   })
   next()
 })
